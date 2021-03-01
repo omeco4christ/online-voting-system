89651b44d87043e3964fdaf53f33804843078cfa
Ome Chukwuemeka <omeco4christ@gmail.comcd>
2021-03-01 12:29:27 +0000

    initial commit

---
+++ Ovschinecherem/src/test/java/com/ovschinecherem/OvschinecheremApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ovschinecherem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OvschinecheremApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
