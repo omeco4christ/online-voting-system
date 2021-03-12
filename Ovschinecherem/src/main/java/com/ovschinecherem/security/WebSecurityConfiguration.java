8daf7ddd8cf79a1c0c997a2cb53746415aa7312a
Ome Chukwuemeka <omeco4christ@gmail.comcd>
2021-03-12 12:13:47 +0000

    5th commit

--- Ovschinecherem/src/main/java/com/ovschinecherem/security/WebSecurityConfiguration.java
+++ Ovschinecherem/src/main/java/com/ovschinecherem/security/WebSecurityConfiguration.java
@@ -29,7 +29,7 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		// TODO Auto-generated method stub
-		http.csrf().disable()
+		http
 		.authorizeRequests()
 			.antMatchers("/").permitAll()
 			.anyRequest().hasRole("USER").and()
