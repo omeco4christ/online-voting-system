87924892ef3c4bfb83b00e25106e40f46598edf4
Ome Chukwuemeka <omeco4christ@gmail.comcd>
2021-03-19 13:30:05 +0000

    6th correction commit

--- Ovschinecherem/src/main/java/com/ovschinecherem/security/WebSecurityConfiguration.java
+++ Ovschinecherem/src/main/java/com/ovschinecherem/security/WebSecurityConfiguration.java
@@ -35,6 +35,7 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 			.anyRequest().hasRole("USER").and()
 		.formLogin()
 			.loginPage("/login")
+			.defaultSuccessUrl("/dashboard")
 			.permitAll()
 			.and()
 		.logout()
